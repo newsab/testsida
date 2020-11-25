964f44680fc5046728ebe5c82d68ddc36a4fdcce
Carl-Fredrik Enerholm <carl-fredrik@CarlFredriksMBP.lan>
2020-11-25 18:02:26 +0100

    lite fix

---
+++ javascript/slideShow.js
@@ -0,0 +1,36 @@
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+
+  slides[slideIndex-1].style.display = "block"; 
+  
+}
+intervall = setInterval(slideIntervall, 9000);
+
+function slideIntervall(){
+plusSlides(1);
+}
+
+function pauseSlide(){
+
+  
+}
+
+
